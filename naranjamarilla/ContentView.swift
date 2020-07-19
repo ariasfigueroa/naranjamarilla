77ef317d4d8e3e8f38ff0563ec7b6672e7b7f911
aldoarias.figueroa <aldo.ariasfigueroa@kony.com>
2020-07-18 20:59:46 -0500

    Initial Commit

---
+++ naranjamarilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  naranjamarilla
+//
+//  Created by aldoarias.figueroa on 7/18/20.
+//  Copyright © 2020 aldoarias.figueroa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
