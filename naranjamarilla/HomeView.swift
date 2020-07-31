3f9cd9b9cce23f10d44ba719736843269bd24d25
aldo arias <ariasfigueroa@gmail.com>
2020-07-31 17:09:13 -0500

    Menu Animation

--- naranjamarilla/HomeView.swift
+++ naranjamarilla/HomeView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct HomeView: View {
     @State var showProfile = false
-    
+    @State var viewState = CGSize.zero
     
     var body: some View {
         ZStack {
@@ -29,8 +29,6 @@ struct HomeView: View {
                             .frame(width: 36, height: 36)
                             .clipShape(Circle())
                     }
-                    
-
                 }
                 .padding(.horizontal)
                 .padding(.top, 30)
@@ -42,20 +40,34 @@ struct HomeView: View {
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .offset(y: self.showProfile ? -450 : 0)
-            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10.0, y: 0, z: 0))
+            .rotation3DEffect(Angle(degrees: showProfile ? Double(self.viewState.height / 12) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
             .scaleEffect(showProfile ? 0.9 : 1)
             .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
             .edgesIgnoringSafeArea(.all)
-            .onTapGesture {
-                if self.showProfile {
-                    self.showProfile.toggle()
-                }
-                
-            }
             
             MenuView()
-                .offset(y: self.showProfile ? 0 : 600)
+                .background(Color.black.opacity(0.001))
+                .offset(y: self.showProfile ? 0 : 700)
+                .offset(y: self.viewState.height)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    self.showProfile.toggle()
+            }
+            .gesture(
+                DragGesture()
+                    .onChanged{ value in
+                        self.viewState = value.translation
+                        if self.viewState.height < -300{
+                            self.viewState.height = -300
+                        }
+                    }
+                .onEnded{ value in
+                    if self.viewState.height > 50 {
+                        self.showProfile = false
+                    }
+                    self.viewState = .zero
+                }
+            )
         }
         
     }
